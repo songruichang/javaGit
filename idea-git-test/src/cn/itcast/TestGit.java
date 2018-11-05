ce199a197f9431407657d6fe2a61e2f414fb7ee6
宋瑞畅 <79491406@qq.com>
2018-11-05 20:35:24 +0800

    将java工程

---
+++ idea-git-test/src/cn/itcast/TestGit.java
@@ -0,0 +1,9 @@
+package cn.itcast;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("321");
+        System.out.println("haha");
+    }
+}
